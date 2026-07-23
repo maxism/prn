d1737dd59a6272acc4542727103b37837b9f99c8
Max Ulianov <maxi@maxism.ru>
2026-07-23 21:16:07 +0300

    wip

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -4,20 +4,12 @@ set -e
 domains=(c-cube.ru suite.c-cube.ru)
 rsa_key_size=4096
 data_path="./certbot"
-email=""   # укажи свой email для уведомлений Let's Encrypt (например admin@c-cube.ru)
+email="fillo.napkin@gmail.com"   # email для уведомлений Let's Encrypt, поменяй если нужен другой
 staging=0  # 1 = тестовые сертификаты Let's Encrypt (не бьют по лимитам), 0 = боевые
 
 if ! command -v docker &> /dev/null; then
   echo "docker не найден, установи docker и docker compose" >&2
   exit 1
-fi
-
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Скачиваю рекомендованные TLS параметры ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
 fi
 
 for domain in "${domains[@]}"; do
@@ -52,8 +44,10 @@ esac
 
 if [ "$staging" != "0" ]; then staging_arg="--staging"; fi
 
+failed_domains=()
 for domain in "${domains[@]}"; do
   echo "### Запрашиваю боевой сертификат Let's Encrypt для $domain ..."
+  set +e
   docker compose run --rm --entrypoint "\
     certbot certonly --webroot -w /var/www/certbot \
       $staging_arg \
@@ -62,8 +56,20 @@ for domain in "${domains[@]}"; do
       --rsa-key-size $rsa_key_size \
       --agree-tos \
       --force-renewal" certbot
+  if [ $? -ne 0 ]; then
+    failed_domains+=("$domain")
+    echo "!!! Не удалось получить сертификат для $domain, продолжаю со следующим доменом"
+  fi
+  set -e
   echo
 done
 
 echo "### Перезагружаю nginx ..."
 docker compose exec nginx nginx -s reload
+
+if [ ${#failed_domains[@]} -ne 0 ]; then
+  echo
+  echo "!!! Не получили сертификаты для: ${failed_domains[*]}"
+  echo "!!! Для этих доменов nginx работает на временном самоподписанном сертификате."
+  exit 1
+fi
